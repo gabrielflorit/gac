1e358d4da0c16c547923fab2b938952047237c91
Gabriel Florit <gabrielflorit@gmail.com>
2016-07-01 17:01:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# gac
+
+Run `git add -A`, `git add -u` & `git commit` with one command. 100% copy-pasted from https://gist.github.com/gka/ff45c5148ad80b53d1d52641006b1194.
+
+## install
+`npm install -g gac`
+
+## usage
+`gac Fix bug XYZ`
+
+## License
+
+MIT © [Gabriel Florit](http://gabrielflorit.github.io)
---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var cmds = [];
+
+if (process.argv.length < 3) {
+	console.log('You need to provide a commit message required!');
+	process.exit(-1);
+}
+
+cmds.push('git add -A');
+cmds.push('git add -u');
+cmds.push('git commit -m "'+process.argv.slice(2).join(' ')+'"');
+
+require('child_process').exec(cmds.join(' && '), function(err, stdout, stderr) {
+  if (err) console.error(err);
+  if (stdout) console.log(stdout);
+  if (stderr) console.error(stderr);
+});
